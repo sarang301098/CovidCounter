a9b7f012cf0eea558c334c991080ebf84113840a
Sarang Patel <sarangp3010@gmail.com>
2021-05-29 16:15:15 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,37 @@
 const url = 'https://covid19.mathdro.id/api';
+let app = angular.module("MyApp", []);
 
-let app = angular.module("MyApp",[]);
-
-app.controller("MyCtrl",($scope,$http)=>{
-
-    $scope.name = "COVID19 GLOBLE COUNTER(LIVE)"; 
-    console.log("loaded");
-    $http.get(url).then((response)=>{
+app.controller("MyCtrl", ($scope, $http) => {
+    $scope.name = "COVID19 GLOBLE COUNTER(LIVE)";
+    $http.get(url).then((response) => {
         console.log(response.data);
         $scope.all_data = response.data;
-        
-    },(error)=>{
+
+    }, (error) => {
         console.log(error);
-        
+
     });
 });
 
 
 
 
-let appc = angular.module("MyAppc",[]);
+let appc = angular.module("MyAppc", []);
 
-appc.controller("MyCtrlc",($scope,$http)=>{
+appc.controller("MyCtrlc", ($scope, $http) => {
 
-    $scope.namec = "COVID19 COUNTRY COUNTER(LIVE)"; 
-    console.log("loaded");
-   
-    $scope.get_c_data = ()=>{
+    $scope.namec = "COVID19 COUNTRY COUNTER(LIVE)";
+    $scope.get_c_data = () => {
         let country = $scope.c;
-        if(country == ""){
+        if (country == "") {
             $scope.c_data = undefined;
             return;
         }
-        $http.get(`${url}/countries/${country}`).then((response)=>{
-             console.log(response.data);
-             $scope.c_data = response.data;
-        }
-        ,(error)=>{
+        $http.get(`${url}/countries/${country}`).then((response) => {
+            console.log(response.data);
+            $scope.c_data = response.data;
+        }, (error) => {
             console.log(error);
-            
-        }); 
-         
+        });
     };
 });
-
-
